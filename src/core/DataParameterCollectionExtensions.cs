36daf770e20965e782dd450e7caac0dc1179ea8c
Goncalo Oliveira <goncalo.a.oliveira@gmail.com>
2022-03-18 15:24:34 +0100

    added command parameter extensions

---
+++ src/core/DataParameterCollectionExtensions.cs
@@ -0,0 +1,65 @@
+namespace System.Data
+{
+    public static class DbCommandParameterExtensions
+    {
+        public static IDbDataParameter AddParameter( this IDbCommand command )
+        {
+            var p = command.CreateParameter();
+
+            command.Parameters.Add( p );
+
+            return ( p );
+        }
+
+        public static IDbDataParameter AddParameterWithValue( this IDbCommand command, string name, object value )
+        {
+            var p = command.CreateParameter();
+
+            p.ParameterName = name;
+            p.Value = value;
+
+            command.Parameters.Add( p );
+
+            return ( p );
+        }
+
+        public static IDbDataParameter AddParameter( this IDbCommand command, string name, DbType dbType )
+        {
+            var p = command.CreateParameter();
+
+            p.ParameterName = name;
+            p.DbType = dbType;
+
+            command.Parameters.Add( p );
+
+            return ( p );
+        }
+
+        public static IDbDataParameter AddParameter( this IDbCommand command, string name, DbType dbType, int size )
+        {
+            var p = command.CreateParameter();
+
+            p.ParameterName = name;
+            p.DbType = dbType;
+            p.Size = size;
+
+            command.Parameters.Add( p );
+
+            return ( p );
+        }
+
+        public static IDbDataParameter AddParameter( this IDbCommand command, string name, DbType dbType, int size, string sourceColumn )
+        {
+            var p = command.CreateParameter();
+
+            p.ParameterName = name;
+            p.DbType = dbType;
+            p.Size = size;
+            p.SourceColumn = sourceColumn;
+
+            command.Parameters.Add( p );
+
+            return ( p );
+        }
+    }
+}
