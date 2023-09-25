157534118762714dc2b787f716ebb97c05084d53
Goncalo Oliveira <goncalo.a.oliveira@gmail.com>
2023-09-25 16:36:07 +0100

    support ctors with service provider resolved instances

--- src/core/DbContextFactory.cs
+++ src/core/DbContextFactory.cs
@@ -22,11 +22,47 @@ namespace System.Data
         {
             var factoryOptions = optionsAccessor.Get( name );
 
-            var context = (IDbContext)Activator.CreateInstance( factoryOptions.ContextType
-                , new object[]
+            // find the constructor with the most parameters
+            var constructor = factoryOptions.ContextType.GetConstructors()
+                .OrderByDescending( c => c.GetParameters().Length )
+                .First();
+
+            // get the constructor parameters
+            var parameters = constructor.GetParameters();
+
+            // populate the constructor arguments
+            var arguments = new List<object>();
+
+            foreach ( var parameter in parameters )
+            {
+                if ( parameter.ParameterType == typeof( DbContextOptions ) )
                 {
-                    factoryOptions.ContextOptions
-                } );
+                    arguments.Add( factoryOptions.ContextOptions );
+                }
+                else if ( parameter.ParameterType == typeof( IServiceProvider ) )
+                {
+                    arguments.Add( provider );
+                }
+                else
+                {
+                    /*
+                    All other types are resolved from the service provider.
+                    */
+
+                    arguments.Add( provider.GetRequiredService( parameter.ParameterType ) );
+                }
+            }
+
+            // create the DbContext
+            var context = (IDbContext)constructor.Invoke( arguments.ToArray() );
+
+            // var context = (IDbContext)Activator.CreateInstance( factoryOptions.ContextType
+            //     , new object[]
+            //     {
+            //         factoryOptions.ContextOptions
+            //     } );
+
+            
 
             return ( context );
         }
