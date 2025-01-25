17b9bc00a7959e01d877018a477487ae1b85c9a6
Goncalo Oliveira <goncalo.a.oliveira@gmail.com>
2025-01-25 11:40:55 +0100

    added WithTransaction extension methods

--- src/core/DbContextTransactionExtensions.cs
+++ src/core/DbContextTransactionExtensions.cs
@@ -5,11 +5,80 @@ namespace System.Data;
 public static class DbContextTransactionExtensions
 {
     /// <summary>
+    /// Executes the specified action within a transaction. If an error occurs, the transaction is rolled back.
+    /// </summary>
+    /// <param name="operation">The operation to execute within the transaction</param>
+    /// <param name="isolationLevel">The isolation level of the transaction</param>
+    /// <param name="cancellationToken">A cancellation token</param>
+    /// <returns>A result indicating whether the transaction was successful</returns>
+    public static DbTransactionResult WithTransactionAsync( this IDbContext context, Action<DbTransaction> operation, IsolationLevel isolationLevel = IsolationLevel.Unspecified, CancellationToken cancellationToken = default )
+    {
+        using var connection = context.GetDbConnection();
+        try
+        {
+            connection.Open();
+
+            using var transaction = connection.BeginTransaction( isolationLevel );
+            try
+            {
+                operation.Invoke( transaction );
+
+                return DbTransactionResult.Success;
+            }
+            catch ( Exception ex )
+            {
+                transaction.Rollback();
+
+                return new DbTransactionResult( false, ex );
+            }
+        }
+        catch ( Exception ex )
+        {
+            return new DbTransactionResult( false, ex );
+        }
+    }
+
+    /// <summary>
+    /// Executes the specified action within a transaction. If an error occurs, the transaction is rolled back.
+    /// </summary>
+    /// <param name="operation">The operation to execute within the transaction</param>
+    /// <param name="isolationLevel">The isolation level of the transaction</param>
+    /// <param name="cancellationToken">A cancellation token</param>
+    /// <returns>A result indicating whether the transaction was successful</returns>
+    public static async Task<DbTransactionResult> WithTransactionAsync( this IDbContext context, Func<DbTransaction, Task> operation, IsolationLevel isolationLevel = IsolationLevel.Unspecified, CancellationToken cancellationToken = default )
+    {
+        using var connection = context.GetDbConnection();
+        try
+        {
+            await connection.OpenAsync( cancellationToken );
+
+            using var transaction = await connection.BeginTransactionAsync( isolationLevel, cancellationToken );
+            try
+            {
+                await operation( transaction );
+
+                return DbTransactionResult.Success;
+            }
+            catch ( Exception ex )
+            {
+                await transaction.RollbackAsync( CancellationToken.None );
+
+                return new DbTransactionResult( false, ex );
+            }
+        }
+        catch ( Exception ex )
+        {
+            return new DbTransactionResult( false, ex );
+        }
+    }
+
+    /// <summary>
     /// Opens a connection and provides a transaction to work with
     /// </summary>
     /// <param name="action">The action to execute with the transaction</param>
     /// <param name="errorAction">The action to execute if an error occurs</param>
     /// <returns>True if the transaction was successful, false otherwise</returns>
+    [Obsolete( "This method has been deprecated and will be removed in a future version. Use the WithTransaction method instead." )]
     public static bool UseTransaction( this IDbContext context, Action<DbTransaction> action, Action<Exception>? errorAction = null )
     {
         using var connection = context.GetDbConnection();
@@ -48,6 +117,7 @@ public static class DbContextTransactionExtensions
     /// <param name="action">The action to execute with the transaction</param>
     /// <param name="errorAction">The action to execute if an error occurs</param>
     /// <returns>True if the transaction was successful, false otherwise</returns>
+    [Obsolete( "This method has been deprecated and will be removed in a future version. Use the WithTransaction method instead." )]
     public static bool UseTransaction( this IDbContext context, IsolationLevel isolationLevel, Action<DbTransaction> action, Action<Exception>? errorAction = null )
     {
         using var connection = context.GetDbConnection();
@@ -86,6 +156,7 @@ public static class DbContextTransactionExtensions
     /// <param name="errorAction">The action to execute if an error occurs</param>
     /// <param name="cancellationToken">The cancellation token</param>
     /// <returns>True if the transaction was successful, false otherwise</returns>
+    [Obsolete( "This method has been deprecated and will be removed in a future version. Use the WithTransactionAsync method instead." )]
     public static async Task<bool> UseTransactionAsync( this IDbContext context, Func<DbTransaction, Task> action, Action<Exception>? errorAction = null, CancellationToken cancellationToken = default )
     {
         using var connection = context.GetDbConnection();
@@ -124,6 +195,7 @@ public static class DbContextTransactionExtensions
     /// <param name="errorAction">The action to execute if an error occurs</param>
     /// <param name="cancellationToken">The cancellation token</param>
     /// <returns>True if the transaction was successful, false otherwise</returns>
+    [Obsolete( "This method has been deprecated and will be removed in a future version. Use the WithTransactionAsync method instead." )]
     public static async Task<bool> UseTransactionAsync( this IDbContext context, Func<DbTransaction, CancellationToken, Task> action, Action<Exception>? errorAction = null, CancellationToken cancellationToken = default )
     {
         using var connection = context.GetDbConnection();
@@ -163,6 +235,7 @@ public static class DbContextTransactionExtensions
     /// <param name="errorAction">The action to execute if an error occurs</param>
     /// <param name="cancellationToken">The cancellation token</param>
     /// <returns>True if the transaction was successful, false otherwise</returns>
+    [Obsolete( "This method has been deprecated and will be removed in a future version. Use the WithTransactionAsync method instead." )]
     public static async Task<bool> UseTransactionAsync( this IDbContext context, IsolationLevel isolationLevel, Func<DbTransaction, Task> action, Action<Exception>? errorAction = null, CancellationToken cancellationToken = default )
     {
         using var connection = context.GetDbConnection();
@@ -202,6 +275,7 @@ public static class DbContextTransactionExtensions
     /// <param name="errorAction">The action to execute if an error occurs</param>
     /// <param name="cancellationToken">The cancellation token</param>
     /// <returns>True if the transaction was successful, false otherwise</returns>
+    [Obsolete( "This method has been deprecated and will be removed in a future version. Use the WithTransactionAsync method instead." )]
     public static async Task<bool> UseTransactionAsync( this IDbContext context, IsolationLevel isolationLevel, Func<DbTransaction, CancellationToken, Task> action, Action<Exception>? errorAction = null, CancellationToken cancellationToken = default )
     {
         using var connection = context.GetDbConnection();
---
+++ src/core/DbContextTransactionResult.cs
@@ -0,0 +1,38 @@
+namespace System.Data;
+
+/// <summary>
+/// Represents the result of a <see cref="DbTransaction"/> operation.
+/// </summary>
+public readonly struct DbTransactionResult
+{
+
+    /// <summary>
+    /// Gets whether the transaction operation succeeded.
+    /// </summary>
+    public bool Succeeded { get; }
+
+    /// <summary>
+    /// Gets the exception that occurred during the transaction operation, if not succeeded.
+    /// </summary>
+    public Exception? Exception { get; }
+
+    internal DbTransactionResult( bool succeeded, Exception? exception = null )
+    {
+        Succeeded = succeeded;
+        Exception = exception;
+    }
+
+    /// <summary>
+    /// Throws an <see cref="InvalidOperationException"/> if the transaction operation did not succeed.
+    /// </summary>
+    /// <exception cref="InvalidOperationException"></exception>
+    public void ThrowIfNotSucceeded()
+    {
+        if ( !Succeeded )
+        {
+            throw new InvalidOperationException( "The transaction operation failed.", Exception );
+        }
+    }
+
+    internal static DbTransactionResult Success { get; } = new( true );
+}
