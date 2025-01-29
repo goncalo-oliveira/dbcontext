b6ba2a0f12a86c9dbb0df77a7d0a3e06e9020fb5
Goncalo Oliveira <goncalo.a.oliveira@gmail.com>
2025-01-29 17:27:45 +0100

    added parameter method extension with DbType

--- src/core/CommandBuilder/DbCommandBuilderExtensions.cs
+++ src/core/CommandBuilder/DbCommandBuilderExtensions.cs
@@ -33,4 +33,18 @@ public static class DbCommandBuilderExtensions
     /// <param name="commandText">The command text to initialize the builder with</param>
     public static IDbCommandBuilder BuildCommand( this DbConnection connection, string commandText )
         => new DbCommandBuilder( connection, commandText );
+
+    /// <summary>
+    /// Adds a parameter to the command
+    /// </summary>
+    /// <param name="name">The parameter name</param>
+    /// <param name="value">The parameter value</param>
+    /// <param name="dbType">The parameter type</param>
+    public static IDbCommandBuilder AddParameter( this IDbCommandBuilder builder, string name, object? value, DbType dbType )
+        => builder.AddParameter( p =>
+        {
+            p.DbType = dbType;
+            p.ParameterName = name;
+            p.Value = value ?? DBNull.Value;
+        } );
 }
